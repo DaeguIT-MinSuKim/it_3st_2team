227dc9920d1d6ff7d177bcd82ee0c2cf6582c633
khj <khjin1231@gmail.com>
2018-02-27 09:10:05 +0900

    sale dto 변수명 변경

--- src/main/java/kr/or/dgit/it_3st_2team/dto/Sale.java
+++ src/main/java/kr/or/dgit/it_3st_2team/dto/Sale.java
@@ -7,23 +7,24 @@ public class Sale { //Foreign key에 대한 변수타입 수정 kim 2018-02-22
 	private String description;
 	private Date sDate;
 	private Date sTime;
-	private Customer cusNo; 
-	private Employee empNo;
-	private Event evnNo;
+	private Customer cus; 
+	private Employee emp;
+	private Event evn;
 	private int sPrice;
 	public Sale() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public Sale(int saleNo, String description, Date sDate, Date sTime, Customer cusNo, Employee empNo, Event evnNo, int sPrice) {
+	public Sale(int saleNo, String description, Date sDate, Date sTime, Customer cus, Employee emp, Event evn,
+			int sPrice) {
 		super();
 		this.saleNo = saleNo;
 		this.description = description;
 		this.sDate = sDate;
 		this.sTime = sTime;
-		this.cusNo = cusNo;
-		this.empNo = empNo;
-		this.evnNo = evnNo;
+		this.cus = cus;
+		this.emp = emp;
+		this.evn = evn;
 		this.sPrice = sPrice;
 	}
 	public int getSaleNo() {
@@ -50,23 +51,23 @@ public class Sale { //Foreign key에 대한 변수타입 수정 kim 2018-02-22
 	public void setsTime(Date sTime) {
 		this.sTime = sTime;
 	}
-	public Customer getCusNo() {
-		return cusNo;
+	public Customer getCus() {
+		return cus;
 	}
-	public void setCusNo(Customer cusNo) {
-		this.cusNo = cusNo;
+	public void setCus(Customer cus) {
+		this.cus = cus;
 	}
-	public Employee getEmpNo() {
-		return empNo;
+	public Employee getEmp() {
+		return emp;
 	}
-	public void setEmpNo(Employee empNo) {
-		this.empNo = empNo;
+	public void setEmp(Employee emp) {
+		this.emp = emp;
 	}
-	public Event getEvnNo() {
-		return evnNo;
+	public Event getEvn() {
+		return evn;
 	}
-	public void setEvnNo(Event evnNo) {
-		this.evnNo = evnNo;
+	public void setEvn(Event evn) {
+		this.evn = evn;
 	}
 	public int getsPrice() {
 		return sPrice;
@@ -76,9 +77,9 @@ public class Sale { //Foreign key에 대한 변수타입 수정 kim 2018-02-22
 	}
 	@Override
 	public String toString() {
-		return String.format(
-				"Sale [saleNo=%s, description=%s, sDate=%s, sTime=%s, cusNo=%s, empNo=%s, evnNo=%s, sPrice=%s]", saleNo,
-				description, sDate, sTime, cusNo, empNo, evnNo, sPrice);
+		return String.format("Sale [saleNo=%s, description=%s, sDate=%s, sTime=%s, cus=%s, emp=%s, evn=%s, sPrice=%s]",
+				saleNo, description, sDate, sTime, cus, emp, evn, sPrice);
 	}
+	
 	
 }
